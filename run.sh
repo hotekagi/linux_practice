c893bc00e58739a96a1ab9a15b8c36ec7e8b9800
hrhrhn <htakagi@Mac.local>
2022-10-31 18:35:32 +0900

    export files from container

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash -v
-
 docker build -t linux_practice .
 docker image prune -f
-docker run -it --rm linux_practice bash
+docker run -it --rm --name linux_practice linux_practice bash
