b217871bca52bcf7857d93cd08ebfc0902c71011
hrhrhn <htakagi@Mac.local>
2022-11-02 22:39:32 +0900

    update for chapter 3

---
+++ src/ch03/multi_load.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+MULTI_CPU=0
+PROG_NAME=$0
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+# multi_load.sh [options] [arg for concurrency]
+# -m enable to use multi cpu
+# 
+
+while getopts "m" OPT ; do
+    case $OPT in
+        m)
+            MULTI_CPU=1
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+CONCURRENCY=$1
+
+if [ $MULTI_CPU -eq 0 ] ; then
+    taskset -p -c 0 $$ > /dev/null
+fi
+
+for ((i=0;i<CONCURRENCY;i++)) do
+    time "${SCRIPT_DIR}/loop.py" &
+done
+
+for ((i=0;i<CONCURRENCY;i++)) do
+    wait
+done
